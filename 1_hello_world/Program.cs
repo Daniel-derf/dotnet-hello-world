fa52307ff0660857e13af46e883f1fce09ecb843
Daniel-derf <danielpardinho321@gmail.com>
2025-07-09 19:27:12 -0300

    instrucao de nivel superior

--- 1_hello_world/Program.cs
+++ 1_hello_world/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+void ShowMessage2()
+{
+  Console.WriteLine("Hello 2");
+}
+
+ShowMessage2();
